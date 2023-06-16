cbaf33cd6dde9af8b3ba2e60f0b491d7e3d3de5b
Anthony Alvarado <anthonydalvarado@gmail.com>
2023-06-16 17:01:04 -0400

    few added things

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -3,7 +3,9 @@ package entity;
 import com.yup.twodgame.GamePanel;
 import com.yup.twodgame.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.IOException;
 
 public class Player extends Entity{
 
@@ -14,12 +16,31 @@ public class Player extends Entity{
         this.gp = gp;
         this.keyH = keyH;
         setDefaultValues();
+        getPlayerImage();
+    }
+
+    public void getPlayerImage(){
+        try{
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/ant_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/ant_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/ant_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/ant_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/ant_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/ant_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/ant_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/ant_right_2.png"));
+
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
     }
 
     public void update(){
