3668adf05f2ed001623e3154709ff4677150a5c9
Anthony Alvarado <anthonydalvarado@gmail.com>
2023-06-16 17:37:50 -0400

    character on screen, moves with wasd

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -5,7 +5,9 @@ import com.yup.twodgame.KeyHandler;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Objects;
 
 public class Player extends Entity{
 
@@ -21,15 +23,14 @@ public class Player extends Entity{
 
     public void getPlayerImage(){
         try{
-            up1 = ImageIO.read(getClass().getResourceAsStream("/player/ant_up_1.png"));
-            up2 = ImageIO.read(getClass().getResourceAsStream("/player/ant_up_2.png"));
-            down1 = ImageIO.read(getClass().getResourceAsStream("/player/ant_down_1.png"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("/player/ant_down_2.png"));
-            left1 = ImageIO.read(getClass().getResourceAsStream("/player/ant_left_1.png"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("/player/ant_left_2.png"));
-            right1 = ImageIO.read(getClass().getResourceAsStream("/player/ant_right_1.png"));
-            right2 = ImageIO.read(getClass().getResourceAsStream("/player/ant_right_2.png"));
-
+            up1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/ant_up_1.png"));
+            up2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/ant_up_2.png"));
+            down1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/ant_down_1.png"));
+            down2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/ant_down_2.png"));
+            left1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/ant_left_1.png"));
+            left2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/ant_left_2.png"));
+            right1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/ant_right_1.png"));
+            right2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/ant_right_2.png"));
         }
         catch(IOException e){
             e.printStackTrace();
@@ -45,21 +46,39 @@ public class Player extends Entity{
 
     public void update(){
         if(keyH.upPressed){
+            direction = "up";
             y -= speed;
         }
         else if(keyH.downPressed){
+            direction = "down";
             y += speed;
         }
         else if(keyH.leftPressed){
+            direction = "left";
             x -= speed;
         }
         else if(keyH.rightPressed){
+            direction = "right";
             x += speed;
         }
     }
 
     public void draw(Graphics2D graphics2D){
-        graphics2D.setColor(Color.white);
-        graphics2D.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
+        switch(direction){
+            case "up":
+                image = up1;
+                break;
+            case "down":
+                image = down1;
+                break;
+            case "left":
+                image = left1;
+                break;
+            case "right":
+                image = right1;
+                break;
+        }
+        graphics2D.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }
